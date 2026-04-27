19680c3a6279d75b3947a31e75bc167709d90d31
jimroxodezi <jimroxodezi@gmail.com>
2026-04-28 00:38:02 +0100

    fixed test

--- models/products_test.go
+++ models/products_test.go
@@ -18,9 +18,4 @@ func TestProductValidation(t *testing.T) {
 		t.Errorf("Expected product to be valid, got error: %s", err)
 	}
 
-	// p2 := Product{}
-	// err = p2.Validate()
-	// if err == nil {
-	// 	t.Error("Expected product to be invalid, got no error")
-	// }
 }
